2eea4e9404f338ccae19cbebe0b3165e83b5e49f
DESKTOP-ABRUA0V\rudi <aseprudi.luqmanul@gmail.com>
2018-05-03 07:35:22 +0700

    rudi

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.menu-icon').click(function(){
+        $('.menu-icon').toggleClass('active');
+        $('.sidebar').toggleClass('active');
+    })
+
+    $('.member-list-choose li:nth-child(this)').click(function(){
+        
+    })
+            
+})
