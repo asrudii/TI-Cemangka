46a5e007c77397d2a08115a4f2dec851eeea5aa2
DESKTOP-ABRUA0V\rudi <aseprudi.luqmanul@gmail.com>
2018-05-10 14:21:45 +0700

    baru deui

--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@ $(document).ready(function(){
         $('.sidebar').toggleClass('active');
     })
 
-    $('.member-list-choose li:nth-child(this)').click(function(){
-        
+
+    $('.member-list-choose li:nth-child(1)').click(function(){
+        $('#Photography').animate({'margin-left': '0px'}, 800);
+    })
+
+    $('.member-list-choose li:nth-child(2)').click(function(){
+        $('#Photography').animate({'margin-left': '-1140px'}, 800);
+    })
+
+    $('.member-list-choose li:nth-child(3)').click(function(){
+        $('#Photography').animate({'margin-left': '-2280px'}, 800);
+    })
+
+    $('.member-list-choose li:nth-child(4)').click(function(){
+        $('#Photography').animate({'margin-left': '-3420px'}, 800);
     })
             
 })
